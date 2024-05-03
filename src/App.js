c8fd849e519ea964fd49a01db084897755250c33
elavarasanjen@gmail.com <elavarasanjen@gmail.com>
2024-05-03 21:04:57 +0530

    Instruction provided

--- src/App.js
+++ src/App.js
@@ -21,3 +21,12 @@ function App() {
 }
 
 export default App;
+
+// "To get started, install the Node modules by running 'npm install'. If the installation fails, ensure that you are in the 'Little-Lemon' directory. If not, navigate to it by using 'cd Little-Lemon'."
+// "Alternatively, you can copy and paste to replace the 'public' and 'src' directories with another React app."
+// "If you encounter any errors while running 'npm start', please install the following dependencies:
+// 'npm install react-router-dom'
+// 'npm install @fortawesome/react-fontawesome @fortawesome/free-brands-svg-icons'
+// 'npm install yup'
+// 'npm install formik'"
+// Happy Learning
